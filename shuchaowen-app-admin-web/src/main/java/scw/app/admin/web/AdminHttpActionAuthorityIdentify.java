423f08c0dc7c08688dddf904ea2e891b568455ed
wcnnkh <247044007@qq.com>
2020-04-23 15:10:51 +0800

    fix

--- shuchaowen-app-admin-web/src/main/java/scw/app/admin/web/AdminHttpActionAuthorityIdentify.java
+++ shuchaowen-app-admin-web/src/main/java/scw/app/admin/web/AdminHttpActionAuthorityIdentify.java
@@ -9,12 +9,10 @@ import scw.mvc.action.authority.HttpActionAuthorityIdentify;
 import scw.mvc.action.manager.HttpAction;
 import scw.mvc.http.HttpChannel;
 import scw.result.ResultFactory;
-import scw.util.result.CommonResult;
-import scw.util.result.CommonResult.AnyResult;
+import scw.util.SimpleResult;
 
 @Configuration(order = Integer.MIN_VALUE)
-public class AdminHttpActionAuthorityIdentify implements
-		HttpActionAuthorityIdentify {
+public class AdminHttpActionAuthorityIdentify implements HttpActionAuthorityIdentify {
 	@Autowired
 	private AdminRoleGroupActionService adminRoleGroupActionService;
 	@Autowired
@@ -22,21 +20,18 @@ public class AdminHttpActionAuthorityIdentify implements
 	@Autowired
 	private ResultFactory resultFactory;
 
-	public CommonResult<Object> identify(HttpChannel httpChannel,
-			HttpAction httpAction, HttpActionAuthority httpActionAuthority)
-			throws Throwable {
-		AdminRole adminRole = adminRoleFactory.getAdminRole(httpChannel,
-				httpAction);
+	public SimpleResult<Object> identify(HttpChannel httpChannel, HttpAction httpAction,
+			HttpActionAuthority httpActionAuthority) throws Throwable {
+		AdminRole adminRole = adminRoleFactory.getAdminRole(httpChannel, httpAction);
 		if (adminRole == null) {
-			return new AnyResult(false, resultFactory.authorizationFailure());
+			return new SimpleResult<Object>(false, resultFactory.authorizationFailure());
 		}
 
-		if (adminRoleGroupActionService.check(adminRole.getGroupId(),
-				httpActionAuthority.getId())) {
-			return new AnyResult(true);
+		if (adminRoleGroupActionService.check(adminRole.getGroupId(), httpActionAuthority.getId())) {
+			return new SimpleResult<Object>(true, null);
 		}
 
-		return new AnyResult(false, resultFactory.error("权限不足"));
+		return new SimpleResult<Object>(false, resultFactory.error("权限不足"));
 	}
 
 }
