1bfb421e5d2f3edfd8e6f1f9e18c7aa0b5c5e495
wcnnkh <247044007@qq.com>
2020-04-28 23:38:01 +0800

    fix

--- shuchaowen-app-admin-web/src/main/java/scw/app/admin/web/AdminActionFilter.java
+++ shuchaowen-app-admin-web/src/main/java/scw/app/admin/web/AdminActionFilter.java
@@ -1,18 +1,23 @@
 package scw.app.admin.web;
 
 import scw.app.admin.pojo.AdminRole;
+import scw.app.admin.service.AdminRoleGroupActionService;
 import scw.beans.annotation.Autowired;
+import scw.core.instance.annotation.Configuration;
 import scw.mvc.action.filter.ActionFilterChain;
 import scw.mvc.action.filter.HttpActionFilter;
 import scw.mvc.action.manager.HttpAction;
 import scw.mvc.http.HttpChannel;
 import scw.util.result.ResultFactory;
 
-public class AdminLoginFilter extends HttpActionFilter {
+@Configuration
+public class AdminActionFilter extends HttpActionFilter {
 	@Autowired
 	private AdminRoleFactory adminRoleFactory;
 	@Autowired
 	private ResultFactory resultFactory;
+	@Autowired
+	private AdminRoleGroupActionService adminRoleGroupActionService;
 
 	@Override
 	protected Object doHttpFilter(HttpChannel channel, HttpAction action,
