1cbdd371dc0720e19748952ec2969a8f81f861ce
wcnnkh <247044007@qq.com>
2020-04-29 21:59:23 +0800

    fix

--- shuchaowen-app-admin-interface/src/main/java/scw/app/admin/service/AdminRoleService.java
+++ shuchaowen-app-admin-interface/src/main/java/scw/app/admin/service/AdminRoleService.java
@@ -4,12 +4,21 @@ import java.util.List;
 
 import scw.app.admin.model.AdminRoleModel;
 import scw.app.admin.pojo.AdminRole;
+import scw.core.GlobalPropertyFactory;
 import scw.core.Pagination;
 import scw.util.result.DataResult;
 
 public interface AdminRoleService {
+	public static String DEFAULT_ADMIN_NAME = GlobalPropertyFactory
+			.getInstance()
+			.getValue("scw.admin.username", String.class, "admin");
+	public static String DEFAULT_PASSWORD = GlobalPropertyFactory.getInstance()
+			.getValue("scw.admin.password", String.class, "123456");
+	public static String DEFAULT_NICKNAME = GlobalPropertyFactory.getInstance()
+			.getValue("scw.admin.nickname", String.class, "超级管理员");
+
 	AdminRole getById(int id);
-	
+
 	AdminRole check(String userName, String password);
 
 	DataResult<AdminRole> create(AdminRoleModel adminRoleModel);
--- shuchaowen-app-admin-web/src/main/java/scw/app/admin/web/AdminActionFilter.java
+++ shuchaowen-app-admin-web/src/main/java/scw/app/admin/web/AdminActionFilter.java
@@ -2,11 +2,15 @@ package scw.app.admin.web;
 
 import scw.app.admin.pojo.AdminRole;
 import scw.app.admin.service.AdminRoleGroupActionService;
+import scw.app.admin.service.AdminRoleService;
 import scw.beans.annotation.Autowired;
 import scw.core.instance.annotation.Configuration;
+import scw.mvc.action.authority.HttpActionAuthority;
+import scw.mvc.action.authority.HttpActionAuthorityManager;
 import scw.mvc.action.filter.ActionFilterChain;
 import scw.mvc.action.filter.HttpActionFilter;
 import scw.mvc.action.manager.HttpAction;
+import scw.mvc.annotation.Authority;
 import scw.mvc.http.HttpChannel;
 import scw.util.result.ResultFactory;
 
@@ -18,15 +22,44 @@ public class AdminActionFilter extends HttpActionFilter {
 	private ResultFactory resultFactory;
 	@Autowired
 	private AdminRoleGroupActionService adminRoleGroupActionService;
+	@Autowired
+	private HttpActionAuthorityManager httpActionAuthorityManager;
 
 	@Override
 	protected Object doHttpFilter(HttpChannel channel, HttpAction action,
 			ActionFilterChain chain) throws Throwable {
-		AdminRole adminRole = adminRoleFactory.getAdminRole(channel, action);
-		if (adminRole == null) {
-			return resultFactory.authorizationFailure();
-		}
+		AdminLogin adminLogin = action.getAnnotatedElement().getAnnotation(
+				AdminLogin.class);
+		Authority authority = action.getMethodAnnotatedElement().getAnnotation(
+				Authority.class);
+		if (adminLogin != null || authority != null) {
+			AdminRole adminRole = adminRoleFactory
+					.getAdminRole(channel, action);
+			if (adminRole == null) {
+				return resultFactory.authorizationFailure();
+			}
 
+			if (adminRole.getUserName().equals(
+					AdminRoleService.DEFAULT_ADMIN_NAME)) {
+				return chain.doFilter(channel, action);
+			}
+
+			if (authority != null) {
+				HttpActionAuthority actionAuthority = httpActionAuthorityManager
+						.getAuthority(action);
+				if (actionAuthority == null) {
+					channel.getLogger().warn("not found autority: {}", action);
+					return chain.doFilter(channel, action);
+				}
+				// 权限判断
+				if (adminRoleGroupActionService.check(adminRole.getGroupId(),
+						actionAuthority.getId())) {
+					return chain.doFilter(channel, action);
+				}
+				
+				return resultFactory.error("权限不足");
+			}
+		}
 		return chain.doFilter(channel, action);
 	}
 
