429f18a97cc12e3c3dea9f2f94c59d13f2686ae5
wcnnkh <247044007@qq.com>
2020-09-03 12:26:07 +0800

    Cleanup

--- scw-app-example/src/main/java/scw/app/example/ExampleApplication.java
+++ scw-app-example/src/main/java/scw/app/example/ExampleApplication.java
@@ -1,10 +1,21 @@
 package scw.app.example;
 
 import scw.application.MainApplication;
+import scw.beans.annotation.Bean;
+import scw.http.server.cors.Cors;
+import scw.http.server.cors.CorsRegistry;
 
 public class ExampleApplication {
 
 	public static void main(String[] args) {
 		MainApplication.run(ExampleApplication.class);
 	}
+	
+	@Bean
+	public CorsRegistry getCorsRegistry(){
+		CorsRegistry corsRegistry = new CorsRegistry();
+		corsRegistry.addMapping("/*", Cors.DEFAULT);
+		return corsRegistry;
+	}
 }
+
