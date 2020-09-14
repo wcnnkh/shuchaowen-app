19533faead37cacee9028de8cc5d12234abed296
wcnnkh <247044007@qq.com>
2020-09-14 16:53:03 +0800

    支持KindUeditor

---
+++ scw-app-core/src/main/java/scw/app/user/controller/KindController.java
@@ -0,0 +1,91 @@
+package scw.app.user.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import scw.app.user.security.RequestUser;
+import scw.http.HttpMethod;
+import scw.http.multipart.FileItem;
+import scw.http.server.MultiPartServerHttpRequest;
+import scw.mvc.annotation.Controller;
+import scw.upload.UploadException;
+import scw.upload.UploadFileItem;
+import scw.upload.kind.KindDirType;
+import scw.upload.kind.KindEditor;
+import scw.upload.kind.KindOrderType;
+
+@Controller(value = "kind")
+public class KindController {
+	private KindEditor kindEditor;
+	private String group;
+
+	public KindController(KindEditor kindEditor) {
+		this.kindEditor = kindEditor;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	@Controller(value = "upload", methods = { HttpMethod.POST, HttpMethod.PUT })
+	public Object upload(RequestUser requestUser, MultiPartServerHttpRequest request, KindDirType dir) {
+		FileItem fileItem = null;
+		for (FileItem item : request.getMultiPartList()) {
+			if (item.isFormField()) {
+				continue;
+			}
+
+			fileItem = item;
+			break;
+		}
+
+		if (fileItem == null) {
+			return error("请选择文件");
+		}
+
+		String group = getGroup();
+		if (group == null) {
+			group = requestUser.getUid() + "";
+		}
+
+		try {
+			String url = kindEditor.upload(group, dir, new UploadFileItem(fileItem));
+			return success(url);
+		} catch (UploadException e) {
+			return error(e.getMessage());
+		} catch (IOException e) {
+			return error(e.getMessage());
+		} finally {
+			request.close();
+		}
+	}
+
+	@Controller(value = "manager", methods = { HttpMethod.GET })
+	public Object manager(RequestUser requestUser, KindDirType dir, String path, KindOrderType order) {
+		String group = getGroup();
+		if (group == null) {
+			group = requestUser.getUid() + "";
+		}
+
+		return kindEditor.manager(group, dir, path, order);
+	}
+
+	private Object success(String url) {
+		Map<String, Object> map = new HashMap<String, Object>(4);
+		map.put("code", 0);
+		map.put("url", url);
+		return map;
+	}
+
+	private Object error(String msg) {
+		Map<String, Object> map = new HashMap<String, Object>(4);
+		map.put("code", 1);
+		map.put("msg", msg);
+		return map;
+	}
+}
