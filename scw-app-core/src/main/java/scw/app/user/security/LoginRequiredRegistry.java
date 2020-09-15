8620f3c5d853a6896514db6f8d20c7298f0d1a24
wcnnkh <247044007@qq.com>
2020-09-15 12:29:10 +0800

    Cleanup

--- scw-app-core/src/main/java/scw/app/user/security/LoginRequiredRegistry.java
+++ scw-app-core/src/main/java/scw/app/user/security/LoginRequiredRegistry.java
@@ -2,17 +2,8 @@ package scw.app.user.security;
 
 import scw.http.server.HttpServiceConfig;
 import scw.http.server.ServerHttpRequest;
-import scw.util.StringMatcher;
 
 public class LoginRequiredRegistry extends HttpServiceConfig<Boolean> {
-
-	public LoginRequiredRegistry() {
-		super();
-	}
-
-	public LoginRequiredRegistry(StringMatcher matcher) {
-		super(matcher);
-	}
 
 	public boolean isLoginRequried(ServerHttpRequest request) {
 		Boolean v = getConfig(request.getPath());
