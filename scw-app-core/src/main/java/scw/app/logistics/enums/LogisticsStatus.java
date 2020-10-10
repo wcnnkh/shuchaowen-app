34a1911c8a27b61a54f2ff2b8d49ea08b3af1bc3
wcnnkh <247044007@qq.com>
2020-10-10 17:40:15 +0800

    物流

---
+++ scw-app-core/src/main/java/scw/app/logistics/enums/LogisticsStatus.java
@@ -0,0 +1,29 @@
+package scw.app.logistics.enums;
+
+/**
+ * 物流状态
+ * 
+ * @author shuchaowen
+ *
+ */
+public enum LogisticsStatus {
+	NOT(100, "未发货"), 
+	ALREADY(200, "已发货/运输中"), 
+	RECEIVED(300, "已收货");
+
+	private final int status;
+	private final String describe;
+
+	private LogisticsStatus(int status, String describe) {
+		this.status = status;
+		this.describe = describe;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public String getDescribe() {
+		return describe;
+	}
+}
